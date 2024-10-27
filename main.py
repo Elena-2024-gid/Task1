e217778478beb686d32e9100c225dced3e145f1e
Elena-2024-gid <semenenko.elena.vladim.1973@gmail.com>
2024-10-27 23:30:08 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+#'1st program'
+print (9**0.5*5)
+#'2nd program'
+print (9.9>9.98 or 1000!=1000.1)
+#'3rd program'
+print (2*2+2)
+print (2*(2+2))
+print ((2*2+2)==(2*(2+2)))
+# '4th program'
+print ((int(10*(float('123.456')))%1230))
